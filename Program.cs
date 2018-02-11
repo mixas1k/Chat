36148aa0c2e96809fa4cf59c26e9e9597b7a1cc2
mixas1k <34314721+mixas1k@users.noreply.github.com>
2018-02-11 14:29:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Chat
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Server server = new Server(2200);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Chat
+{
+    class Server
+    {
+        public Server(int port)
+        {
+            _port = port;
+
+            // Получаем информацию о локальном компьютере
+            IPHostEntry localMachineInfo = Dns.GetHostEntry(Dns.GetHostName());
+            Console.WriteLine("Host: {0}", Dns.GetHostName());
+
+            IPEndPoint myAddress = new IPEndPoint(localMachineInfo.AddressList[0], _port);
+            Console.WriteLine("Address: {0}", myAddress.Address);
+
+            // Инициализируем серверный сокет
+            _serverSocket = new Socket(myAddress.Address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Привязываем его к нашему адрессу
+            _serverSocket.Bind(myAddress);
+
+            // Начинаем прослушивание
+            _serverSocket.Listen((int)SocketOptionName.MaxConnections);
+        }
+
+        private int _port;
+        private Socket _serverSocket;
+    }
+}
